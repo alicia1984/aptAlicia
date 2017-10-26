4554e7e9dfa270aafabc60ebb393597a9b21856d
alicia1984 <alicia28031@hotmail.com>
2017-10-25 21:12:01 -0300

    echo  ejercicios  con operaciones  basicas  3

---
+++ src/aptalicia/AptAlicia.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aptalicia;
+
+/**
+ *
+ * @author LP 13
+ */
+public class AptAlicia {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        // TODO code application logic hereç
+        int numero;//Declaracion de variable tipo entero
+      double numero2;//Declaracion variable tipo double
+     
+      
+       numero=33;//Acabo de inicalizar la variable numero
+      numero2=41;//inicializo la variable numero2
+      
+      //En este bloque vamos a usar las variables
+      System.out.println ("Suma de numero y numero2: "+(numero+numero2));
+       System.out.println ("Resta de numero y numero2: "+(numero-numero2));
+       System.out.println ("Multiplicacion de numero y numero2: "+(numero*numero2));
+       System.out.println ("Division de numero y numero2: "+(numero/numero2));
+        System.out.println ("Residuo de numero y numero2: "+(numero%numero2));
+    }
+    
+}
